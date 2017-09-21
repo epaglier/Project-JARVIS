6daba8131e57e5b9d6f2fa94a0a1e7a3f05d78aa
Evan <epaglier@purdue.edu>
2017-09-21 19:22:31 +0000

    Added mycroft files.

---
+++ mycroft/messagebus/service/main.py
@@ -0,0 +1,64 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+import tornado.ioloop as ioloop
+import tornado.web as web
+import tornado.autoreload as autoreload
+
+from mycroft.configuration import ConfigurationManager
+from mycroft.messagebus.service.ws import WebsocketEventHandler
+from mycroft.util import validate_param
+from mycroft.lock import Lock  # creates/supports PID locking file
+
+
+__author__ = 'seanfitz', 'jdorleans'
+
+settings = {
+    'debug': True
+}
+
+
+def main():
+    import tornado.options
+    lock = Lock("service")
+    tornado.options.parse_command_line()
+
+    def reload_hook():
+        """ Hook to release lock when autoreload is triggered. """
+        lock.delete()
+
+    tornado.autoreload.add_reload_hook(reload_hook)
+
+    config = ConfigurationManager.get().get("websocket")
+
+    host = config.get("host")
+    port = config.get("port")
+    route = config.get("route")
+    validate_param(host, "websocket.host")
+    validate_param(port, "websocket.port")
+    validate_param(route, "websocket.route")
+
+    routes = [
+        (route, WebsocketEventHandler)
+    ]
+    application = web.Application(routes, **settings)
+    application.listen(port, host)
+    ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ mycroft/util/log.py
@@ -0,0 +1,46 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+import json
+import logging
+
+from os.path import isfile
+from mycroft.util.json_helper import load_commented_json
+
+SYSTEM_CONFIG = '/etc/mycroft/mycroft.conf'
+
+__author__ = 'seanfitz'
+
+log_level = "DEBUG"
+
+if isfile(SYSTEM_CONFIG):
+    config = load_commented_json(SYSTEM_CONFIG)
+    log_level = config.get("log_level", "DEBUG")
+
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.getLevelName(log_level))
+logger = logging.getLogger("MYCROFT")
+
+
+def getLogger(name="MYCROFT"):
+    """
+    Get a python logger
+
+    :param name: Module name for the logger
+
+    :return: an instance of logging.Logger
+    """
+    return logging.getLogger(name)
