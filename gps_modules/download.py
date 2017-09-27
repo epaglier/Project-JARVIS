9159f45dc74079fc51804e68539812704a603846
Evan <epaglier@purdue.edu>
2017-09-27 03:42:40 +0000

    Adapted upload and download to work with Location object

--- gps_modules/download.py
+++ gps_modules/download.py
@@ -1,13 +1,20 @@
 import mysql.connector
+from hurry.filesize import size
 
 cnx = mysql.connector.connect(user='pi', password='sudo', database='test')
 
 cursor = cnx.cursor()
 querry = ("SELECT * FROM gps LIMIT 0, 100")
 cursor.execute(querry)
-
 count = 0
+
 for (time,longitude,latitude,velocity) in cursor:
-    print "time: {}, latitude: {}, longitude: {}\n".format(time, longitude,latitude)
+    print "time: {}, latitude: {}, longitude: {}, velocity: {} m/s\n".format(time, latitude, longitude, velocity)
     count = count +1
-print count
+print "Number of entries: " + str(count)
+
+cursor2 = cnx.cursor()
+cursor2.execute("SELECT data_length FROM information_schema.TABLES WHERE table_name = \"gps\"")
+
+for (data_length) in cursor2:
+    print "size: " + size(int(data_length[0]))
--- gps_modules/upload.py
+++ gps_modules/upload.py
@@ -4,14 +4,16 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user='pi', password='sudo', database='test')
 cursor = cnx.cursor()
-
 while True:
-    stringToSend = ("INSERT INTO gps (time, longitude, latitude, velocity) VALUES (%s, %s, %s, %s)")
-    latitude = str(gps.getLatitude())
+    stringToSend = ("INSERT INTO gps (time, latitude, longitude, velocity) VALUES (%s, %s, %s, %s)")
+    loc = gps.getLocation()
+    latitude = str(loc.getLatitude())
     print latitude
-    longitude = str(gps.getLongitude())
+    longitude = str(loc.getLongitude())
     print longitude
-    data= (datetime.datetime.now(), longitude, latitude, str(gps.getVelocity()))
+    velocity = str(loc.getVelocity())
+    print velocity
+    data= (datetime.datetime.now(), latitude, longitude, velocity)
     cursor.execute(stringToSend, data)
     cnx.commit()
     print "uploaded"
