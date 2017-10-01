af768fa26311ddce098b07d4ee1fbe3772f52c6f
Daniel Krolopp <dkrolopp@purdue.edu>
2017-10-01 18:11:50 -0400

    GPS bug fix. Fixed wifid and gpsd.

--- daemons/gpsd.py
+++ daemons/gpsd.py
@@ -10,17 +10,17 @@ sys.path.insert(0, '/home/' + user + '/mycroft-core/gps_modules/')
 import gps
 
 signal = False
-
+#DEBUG signal = True
 while(True):
     time.sleep(5)
     loc = gps.getLocation()
     if loc.getFix() and not signal:
         # no signal -> signal
-        #TODO speak = subprocess.check_output(["mimic", "-t", "GPS signal established."])
+        subprocess.check_output(["../mimic/mimic", "-t", "\"GPS signal established.\""])
         signal = True
         print "[gpsd] GPS signal established."
     elif not loc.getFix() and signal:
         # signal -> no signal
-        #TODO speak = subprocess.check_output(["mimic", "-t", "GPS signal lost."])
+        subprocess.check_output(["../mimic/mimic", "-t", "\"GPS signal lost.\""])
         signal = False
         print "[gpsd] GPS signal lost."
