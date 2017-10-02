1fb17c96d687740bec1fa76b14a14cfe7b01e617
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2017-10-01 22:12:31 -0400

    Added the ability to search for songs by name.

--- music/music_player.py
+++ music/music_player.py
@@ -4,7 +4,19 @@ class Song:
         self.album = album
         self.title = title
 
+    def to_string(self):
+        return self.artist + ': ' + self.title + ' [' + self.album + ']'
+
 
 class MusicAccessException(Exception):
     def __init__(self, message):
         super(Exception).__init__(self, message)
+
+
+def find_songs_by_name(song_list, title_substring):
+    title_substring = title_substring.lower()
+    matches = []
+    for song in song_list:
+        if title_substring in song.title.lower():
+            matches.append(song)
+    return matches
