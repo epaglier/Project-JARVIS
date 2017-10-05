b5b9253de4df296806205299f2460660814ff3a1
Gus Vroustouris <gvrousto@purdue.edu>
2017-10-05 15:08:08 -0400

    beginning mycroft integration

---
+++ mycroft/guscalendar/__init__.py
@@ -0,0 +1,20 @@
+from adapt.intent import IntentBuilder
+
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'gvrousto'
+
+LOGGER = getLogger(__name__)
+
+class guscalendarSkill(MycroftSkill):
+    def __init__(self):
+
+    def initialize(self):
+
+    def handle_guscalendar_intent(self):
+
+    def stop(self):
+
+def create_skill():
+    
