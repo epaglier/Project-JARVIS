5ddbf9363a77d0937b3b5f6b8cd2ddcdadde3f70
Daniel Krolopp <dkrolopp@purdue.edu>
2017-10-24 10:30:43 -0400

    Fully integrated new GPS functionality into gps.py. All changes should be transparent to other developers.

--- gps_modules/test-2.py
+++ gps_modules/test-2.py
@@ -1,10 +1,16 @@
 import time
-import new_gps
+import gps
 
-
-time.sleep(1) #wait for NMEA sentences to arrive
-N = new_gps.getLocation()
+N = gps.getLocation()
 
 print "Velocity: " + repr(N.getVelocity())
 print "Fix: " + repr(N.getFix())
 
+print "sleeping 2 seconds"
+
+time.sleep(2)
+
+print "getting a new location"
+new = gps.getLocation()
+print "Velocity: " + repr(new.getVelocity())
+time.sleep(.1)
