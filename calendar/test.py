5ad61d083409239cbfc1ce85be778f8f65ec2cd1
Gus Vroustouris <gvrousto@purdue.edu>
2017-11-03 14:15:12 -0400

    removed some things

--- calendar/test.py
+++
@@ -1,9 +0,0 @@
-import calendar_api
-
-CM = calendar_api.Calendar_Mutator()
-numEvents = 7
-CM.getXEvents(numEvents)
-location = 'test the calendar insert'
-description = 'asdf'
-event = 'test'
-CM.setEvent(event, description,event)
---
+++ mycroft/gmail/__init__.py
@@ -0,0 +1,48 @@
+from adapt.intent import IntentBuilder
+from mycroft.messagebus.message import Message
+
+from mycroft.configuration import ConfigurationManager
+from mycroft.util import record, play_mp3
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from oauth2client import file
+import datetime
+import calendar
+import time
+
+from os.path import dirname, abspath, join, expanduser
+import sys
+
+import json
+from json import JSONEncoder
+def get_credentials():
+    """Gets valid user credentials from storage.
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'calendar-python-quickstart.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
