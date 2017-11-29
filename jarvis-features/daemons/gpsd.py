9267ff6a6e44a28f6f28036994657463e6120fe4
Daniel Krolopp <dkrolopp@purdue.edu>
2017-11-28 21:48:51 -0500

    Fixed a bug in gpsd.py

--- jarvis-features/daemons/gpsd.py
+++ jarvis-features/daemons/gpsd.py
@@ -16,11 +16,11 @@ while(True):
     loc = gps.getLocation()
     if loc.getFix() and not signal:
         # no signal -> signal
-        subprocess.check_output(["../../mycroft-core/mimic/mimic", "-t", "\"GPS signal established.\""])
+        subprocess.check_output(["/bin/mimic/mimic", "-t", "\"GPS signal established.\""])
         signal = True
         print "[gpsd] GPS signal established."
     elif not loc.getFix() and signal:
         # signal -> no signal
-        subprocess.check_output(["../../mycroft-core/mimic/mimic", "-t", "\"GPS signal lost.\""])
+        subprocess.check_output(["/bin/mimic/mimic", "-t", "\"GPS signal lost.\""])
         signal = False
         print "[gpsd] GPS signal lost."
