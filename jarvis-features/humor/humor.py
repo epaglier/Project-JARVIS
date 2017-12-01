e4751f8e86091b9fa49a06a0d590420d2b94ebfb
Daniel Krolopp <dkrolopp@purdue.edu>
2017-12-01 16:01:11 -0500

    Humor update

--- jarvis-features/humor/humor.py
+++ jarvis-features/humor/humor.py
@@ -6,25 +6,26 @@ import subprocess
 import random
 
 # Used as a cache when no network is available
-cache = None
+cache = []
 
 # Used to randomly select a joke source
 rand = random.randint(0, 2)
+
 temp = subprocess.check_output(["curl", "http://api.yomomma.info/"])
-print temp
-if "curl: (6) Could not resolve host: api.yomomma.info" == temp
-    if cache.len() > 0:
-        joke = cache.pop(cache.len() - 1)
+if "curl: (6) Could not resolve host: api.yomomma.info" == temp:
+    if len(cache) > 0:
+        joke = cache.pop(len(cache) - 1)
         subrocesss.check_output(["/bin/mimic/mimic", "-t", joke])
     else:
         # No wifi/API unavailable
         subrocesss.check_output(["/bin/mimic/mimic", "-t", "I can't make a joke now, sorry."])
         sys.exit() # This could be wrong and cause a crash
-while cache.len() < 50:
+while len(cache) < 5:
     if rand == 0:
         with open(os.devnull, 'w') as devnull:
-            joke = subprocess.check_output(["curl", "-H", "\"Accept: text/plain\"", "https://icanhazdadjoke.com/"], stderr=devnull)
-
+            joke = subprocess.check_output(["curl", "-H", "\"Accept: text/plain\"", \
+                    "https://icanhazdadjoke.com/"])
+            cache.append(joke)
     elif rand == 1:
         response = urllib2.urlopen("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke").read()
         parsed_data = json.loads(response)
@@ -34,8 +35,10 @@ while cache.len() < 50:
     else:
         response = urllib2.urlopen("http://api.yomomma.info/").read()
         parsed_data = json.loads(response)
-        subprocess.check_output(["/bin/mimic/mimic", "-t", parsed_data['joke']])
-        cache.append(joke)
+        #subprocess.check_output(["/bin/mimic/mimic", "-t", parsed_data['joke']])
+        cache.append(parsed_data['joke'])
 
 
-subprocess.check_output(["/bin/mimic/mimic", "-t", cache.pop(cache.len() - 1)])
+subprocess.check_output(["/bin/mimic/mimic", "-t", cache.pop(len(cache) - 1)])
+print cache.pop(len(cache) - 1)
+#print "cache length: " + repr(len(cache))
